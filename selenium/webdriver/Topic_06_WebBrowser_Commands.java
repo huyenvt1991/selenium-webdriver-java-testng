ed06e03ee43256e87875cbb11f1c5286c08e7ed3
DESKTOP-AVSL1GB\User <minhha1.bhsoft@gmail.com>
2023-11-30 18:19:27 +0700

    Update

--- selenium/webdriver/Topic_06_WebBrowser_Commands.java
+++ selenium/webdriver/Topic_06_WebBrowser_Commands.java
@@ -13,8 +13,11 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 
 public class Topic_06_WebBrowser_Commands {
     // Các câu lệnh để thao tác với Browser
@@ -41,17 +44,17 @@ public class Topic_06_WebBrowser_Commands {
         // Nếu không khởi tạo sẽ gặp lỗi: NullPointerException
         // 1 biến chưa được khởi tạo nhưng đã được gọi ra để sử dụng
 //        driver = new FirefoxDriver();
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(); // **
 //        driver = new EdgeDriver();
 //        driver = new InternetExplorerDriver();
 //        driver = new SafariDriver();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));  // **
+        driver.manage().window().maximize(); // **
     }
 
     @Test
-    public void TC_01_() {
+    public void TC_01_() throws MalformedURLException {
         // m - method
         //f - field
         //c - class
@@ -61,24 +64,24 @@ public class Topic_06_WebBrowser_Commands {
         //@ - annotation
 
         // Mở ra 1 URL bất kỳ
-        driver.get("https://www.facebook.com/");
+        driver.get("https://www.facebook.com/"); // **
 
         System.out.print("Tab ID = " + driver.getWindowHandle());
 
         // Nếu như có 1 tab thì tính năng tự quit
         // Nhiều hơn thì sẽ đóng cái đang active
-        driver.close();
+        driver.close(); // *
 
         // Đóng browser (bao nhiêu tab đóng hết)
-        driver.quit();
+        driver.quit(); // **
 
-        WebElement emailAddressTextbox = driver.findElement(By.id("email"));
+        WebElement emailAddressTextbox = driver.findElement(By.id("email")); // **
 
         // Không được tìm thấy ==> ko bị fail ==> trả về 1 list rỗng (0 element)
-        List<WebElement> checkboxes = driver.findElements(By.xpath(""));
+        List<WebElement> checkboxes = driver.findElements(By.xpath("")); // **
 
         // Dùng để lấy ra URL của màn hình/ page hiện tại đang đứng
-        driver.getCurrentUrl();
+        driver.getCurrentUrl(); // *
         Assert.assertTrue(driver.getCurrentUrl().contains(""));
 
         // Lấy ra page source HTML/CSS/JS của page hiện tại
@@ -90,17 +93,17 @@ public class Topic_06_WebBrowser_Commands {
 
         // Lấy ra ID của tab hiện tại
         // Dùng cho bài Handle Window/Tab
-        driver.getWindowHandle();
-        driver.getWindowHandles();
+        driver.getWindowHandle(); // *
+        driver.getWindowHandles(); // *
 
         // Dùng cho bài Cookies - Framework
-        driver.manage().getCookies();
+        driver.manage().getCookies(); // *
 
         // Get ra những logs ở Dev Tool
-        driver.manage().logs().get(LogType.DRIVER);
+        driver.manage().logs().get(LogType.DRIVER); // *
 
         // Apply cho việc tìm element
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30)); // **
 
         // Chờ cho page được load xong
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
@@ -118,7 +121,7 @@ public class Topic_06_WebBrowser_Commands {
         driver.manage().window().fullscreen();
 
         // Hay dùng
-        driver.manage().window().maximize();
+        driver.manage().window().maximize(); // **
         driver.manage().window().minimize();
 
         // Test giao diện - GUI
@@ -132,9 +135,39 @@ public class Topic_06_WebBrowser_Commands {
         driver.manage().window().setPosition(new Point(0, 0));
         driver.manage().window().getPosition();
 
-        driver.navigate();
+        // Điều hướng trang web
+        driver.navigate().back();
+        driver.navigate().refresh();
+        driver.navigate().forward();
 
-        driver.switchTo();
+        // Hỗ trợ tốt hơn khi thao tác history của web page (back/forward)
+        driver.navigate().to("https://www.facebook.com/");
+        driver.navigate().to(new URL("https://www.facebook.com/"));
+
+        // Alert/ Window (Tab)/ Frame (iFrame) // *
+        driver.switchTo().alert().accept();
+        driver.switchTo().alert().dismiss();
+        driver.switchTo().alert().getText();
+        driver.switchTo().alert().sendKeys("Test");
+
+        // Handle Window/Tab // *
+        String homePageWindowID = driver.getWindowHandle();
+        driver.switchTo().window(homePageWindowID);
+
+        Set<String> allWindowIDs = driver.getWindowHandles();
+
+        // Handle Frame(Iframe) // *
+        driver.switchTo().frame(0);
+        driver.switchTo().frame("123456");
+        driver.switchTo().frame(driver.findElement(By.id("frame")));
+
+        // Switch về HTML chứa frame trước đó // *
+        driver.switchTo().defaultContent();
+
+        // Từ frame bên trong đi ra frame bên ngoài chứa nó // *
+        driver.switchTo().parentFrame();
+
+
     }
 
     @Test
---
+++ selenium/webdriver/Topic_07_WebElement_Commands.java
@@ -0,0 +1,41 @@
+package webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_07_WebElement_Commands {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_() {
+    }
+
+    @Test
+    public void TC_02_() {
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
