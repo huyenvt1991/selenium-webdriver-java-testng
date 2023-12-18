5e1c3ba160a8e69de313f008aeb5738c4a4a9574
huyenvt <huyenvothi588@gmail.com>
2023-12-18 23:58:37 +0700

    Update

--- selenium/webdriver/Topic_08_Textbox_Textarea.java
+++ selenium/webdriver/Topic_08_Textbox_Textarea.java
@@ -1,10 +1,12 @@
 package webdriver;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -14,10 +16,20 @@ import java.time.Duration;
 public class Topic_08_Textbox_Textarea {
     WebDriver driver;
 
+    public void sleepInSeconds(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     WebElement myAccount_link;
     WebElement email_field;
     WebElement password_field;
     WebElement login_btn;
+    WebElement requiredField_errorMsg_email;
+    WebElement requiredField_errorMsg_password;
 
 
     @BeforeClass
@@ -27,13 +39,28 @@ public class Topic_08_Textbox_Textarea {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.manage().window().maximize();
         driver.get("http://live.techpanda.org/");
+        sleepInSeconds(3);
 
-        myAccount_link = driver.findElement();
+        myAccount_link = driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']"));
     }
 
     @Test
     public void TC_01_Empty_Email_And_Password() {
+        myAccount_link.click();
+        sleepInSeconds(2);
 
+        email_field = driver.findElement(By.cssSelector("input#email"));
+        password_field = driver.findElement(By.cssSelector("input#pass"));
+        login_btn = driver.findElement(By.cssSelector("button#send2"));
+        sleepInSeconds(2);
+
+        requiredField_errorMsg_email = driver.findElement(By.cssSelector("div#advice-required-entry-email"));
+        requiredField_errorMsg_password = driver.findElement(By.cssSelector("div#advice-required-entry-pass"));
+
+        Assert.assertTrue(requiredField_errorMsg_email.isDisplayed());
+        Assert.assertEquals(requiredField_errorMsg_email.getText(), "This is a required field.");
+        Assert.assertTrue(requiredField_errorMsg_password.isDisplayed());
+        Assert.assertEquals(requiredField_errorMsg_password.getText(), "This is a required field.");
     }
 
     @Test
