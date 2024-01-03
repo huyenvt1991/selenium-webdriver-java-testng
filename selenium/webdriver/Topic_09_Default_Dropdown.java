a0d9f64f4f087f8661adff1d3bbb668caf48449e
DESKTOP-AVSL1GB\User <minhha1.bhsoft@gmail.com>
2024-01-03 18:33:07 +0700

    Update

--- selenium/webdriver/Topic_09_Default_Dropdown.java
+++ selenium/webdriver/Topic_09_Default_Dropdown.java
@@ -3,6 +3,7 @@ package webdriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -27,17 +28,50 @@ public class Topic_09_Default_Dropdown {
         return "minhha" + random.nextInt(99999) + "@gmail.com";
     }
 
+    public void sleepInSeconds(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public String firstName_text = "Minh";
     public String lastName_text = "Hoang";
 
     public String email_text = getEmailAddress();
-
+    public String password_text = "Hoangducanh2003@";
 
     @Test
     public void TC_01_Register() {
         driver.findElement(By.cssSelector("a[class='ico-register']")).click();
-        driver.findElement(By.cssSelector("input[id='FirstName']")).sendKeys("Minh");
-        driver.findElement(By.cssSelector("a[class='ico-register']")).click();
+        driver.findElement(By.cssSelector("input[id='FirstName']")).sendKeys(firstName_text);
+        driver.findElement(By.cssSelector("input[id='LastName']")).sendKeys(lastName_text);
+        driver.findElement(By.cssSelector("input[id='Email']")).sendKeys(email_text);
+
+        Select day_dropdown = new Select(driver.findElement(By.name("DateOfBirthDay")));
+        day_dropdown.selectByVisibleText("21");
+        Assert.assertFalse(day_dropdown.isMultiple());
+        Assert.assertEquals(day_dropdown.getOptions(), 32);
+
+        Select month_dropdown = new Select(driver.findElement(By.name("DateOfBirthMonth")));
+        month_dropdown.selectByVisibleText("January");
+        Assert.assertFalse(month_dropdown.isMultiple());
+        Assert.assertEquals(day_dropdown.getOptions(), 32);
+
+        Select year_dropdown = new Select(driver.findElement(By.name("DateOfBirthYear")));
+        year_dropdown.selectByVisibleText("1996");
+        Assert.assertFalse(year_dropdown.isMultiple());
+
+//        new Select(driver.findElement(By.name("DateOfBirthDay"))).selectByVisibleText("21");
+//        new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText("January");
+//        new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText("1996");
+
+
+        driver.findElement(By.cssSelector("input[id='Password']")).sendKeys(password_text);
+        driver.findElement(By.cssSelector("input[id='ConfirmPassword']")).sendKeys(password_text);
+
+        driver.findElement(By.id("register-button")).click();
+        sleepInSeconds(3);
 
     }
 
