d515644e9d68c5c89fe4b9b90c4d5713fac2802a
huyenvt <huyenvothi588@gmail.com>
2024-01-07 22:14:35 +0700

    Update

--- selenium/webdriver/Topic_09_Default_Dropdown.java
+++ selenium/webdriver/Topic_09_Default_Dropdown.java
@@ -44,6 +44,11 @@ public class Topic_09_Default_Dropdown {
     public String email_text = getEmailAddress();
     public String password_text = "Hoangducanh2003@";
 
+    public String day_text = "21";
+    public String month_text = "January";
+    public String year_text = "1996";
+
+
     @Test
     public void TC_01_Register() {
         driver.findElement(By.cssSelector("a[class='ico-register']")).click();
@@ -52,22 +57,22 @@ public class Topic_09_Default_Dropdown {
         driver.findElement(By.cssSelector("input[id='Email']")).sendKeys(email_text);
 
         Select day_dropdown = new Select(driver.findElement(By.name("DateOfBirthDay")));
-        day_dropdown.selectByVisibleText("21");
-        Assert.assertFalse(day_dropdown.isMultiple());
-        List<WebElement> dayOptions = day_dropdown.getOptions();
-        Assert.assertEquals(dayOptions.size(), 32);
+        day_dropdown.selectByVisibleText(this.day_text);
+        Assert.assertFalse(day_dropdown.isMultiple()); // Ít dùng
+        List<WebElement> dayOptions = day_dropdown.getOptions(); // Ít dùng
+        Assert.assertEquals(dayOptions.size(), 32); // Ít dùng
 
         Select month_dropdown = new Select(driver.findElement(By.name("DateOfBirthMonth")));
-        month_dropdown.selectByVisibleText("January");
-        Assert.assertFalse(month_dropdown.isMultiple());
-        List<WebElement> monthOptions = month_dropdown.getOptions();
-        Assert.assertEquals(monthOptions.size(), 13);
+        month_dropdown.selectByVisibleText(this.month_text);
+        Assert.assertFalse(month_dropdown.isMultiple()); // Ít dùng
+        List<WebElement> monthOptions = month_dropdown.getOptions(); // Ít dùng
+        Assert.assertEquals(monthOptions.size(), 13); // Ít dùng
 
         Select year_dropdown = new Select(driver.findElement(By.name("DateOfBirthYear")));
-        Assert.assertFalse(year_dropdown.isMultiple());
-        List<WebElement> yearOptions = year_dropdown.getOptions();
-        Assert.assertEquals(yearOptions.size(), 112);
-        year_dropdown.selectByVisibleText("1996");
+        Assert.assertFalse(year_dropdown.isMultiple()); // Ít dùng
+        List<WebElement> yearOptions = year_dropdown.getOptions(); // Ít dùng
+        Assert.assertEquals(yearOptions.size(), 112); // Ít dùng
+        year_dropdown.selectByVisibleText(this.year_text);
 
 
 //        new Select(driver.findElement(By.name("DateOfBirthDay"))).selectByVisibleText("21");
@@ -81,11 +86,29 @@ public class Topic_09_Default_Dropdown {
         driver.findElement(By.id("register-button")).click();
         sleepInSeconds(3);
 
-//        Assert.assertTrue(driver.findElement());
+        Assert.assertTrue(driver.findElement(By.className("result")).getText().contains("Your registration completed"));
     }
 
     @Test
     public void TC_02_Login() {
+        driver.findElement(By.className("ico-login")).click();
+        sleepInSeconds(2);
+
+        driver.findElement(By.id("Email")).sendKeys(this.email_text);
+        driver.findElement(By.id("Password")).sendKeys(this.password_text);
+        driver.findElement(By.xpath("//button[@class='button-1 login-button']")).click();
+
+        driver.findElement(By.className("ico-account")).click();
+        sleepInSeconds(2);
+
+        Assert.assertEquals(driver.findElement(By.id("FirstName")).getAttribute("value"), this.firstName_text);
+        Assert.assertEquals(driver.findElement(By.id("LastName")).getAttribute("value"), this.lastName_text);
+
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthDay"))).getFirstSelectedOption().getText(), this.day_text);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthMonth"))).getFirstSelectedOption().getText(), this.month_text);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthYear"))).getFirstSelectedOption().getText(), this.year_text);
+
+        Assert.assertEquals(driver.findElement(By.id("Email")).getAttribute("value"), this.email_text);
 
     }
 
